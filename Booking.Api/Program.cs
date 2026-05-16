d37abe330d2bfb4efe0f8ce90c00026daf22f263
Ilya <125735682+SUSSSSSSSSSSSSSSY@users.noreply.github.com>
2026-05-16 13:37:24 +0300

    Patched Origins

--- Booking.Api/Program.cs
+++ Booking.Api/Program.cs
@@ -185,7 +185,8 @@ builder.Services.AddCors(options =>
         policy
             .WithOrigins(
                 "http://localhost:3000",
-                "http://localhost:5173"
+                "http://localhost:5173",
+                "http://localhost:8081"
             //  URL Azure Static Web Apps frontend
             // "https://frontend.azurestaticapps.net"
             )
